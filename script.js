6d2dad90a02917fadde598d28bbb9848794d1f8b
Glitch (glitch-hello-website) <none>
2023-05-30 02:42:58 +0000

    🛳🍝 Checkpoint ./.glitch-assets:46454182/3481 ./index.html:46454182/14270 ./style.css:46454182/7340 ./script.js:46454182/309

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.from('.boxes', {y:-20,  duration: 3, delay: 1.5, opacity: 0, stagger: .3, repeat: -1, repeatDelay: 3})
+gsap.from('.photoOne', {y: -200, ease: 'bounce', duration: 5, delay: 2, opacity: 0})
+
+gsap.to('.text', {
+text: "a web developer!", 
+duration: 3,
+repeat: -1,
+ease: "power1.in",
+yoyo: true,
+delay: 2
+})
+
